7835b0e860e0814dcd51b39a9f1ae956e2a6c174
FauziFuzzy <fauzi_zulfadli@yahoo.com>
2025-08-22 20:59:39 +0800

    feat: Add comprehensive widget tests for anchored sheets and remove basic test

--- test/basic_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Package can be imported', () {
-    // Basic test to ensure the package structure is valid
-    expect(true, isTrue);
-  });
-}
